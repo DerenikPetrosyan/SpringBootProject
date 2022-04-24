2eeae28418b1fd79147638693ecd748713661d0a
DerenikPetrosyan <deren_petrosyan@mail.ru>
2022-04-24 21:48:38 +0400

    new commite1

--- src/main/java/am/basic/repository/UserRepository.java
+++ src/main/java/am/basic/repository/UserRepository.java
@@ -2,6 +2,7 @@ package am.basic.repository;
 
 import am.basic.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +12,7 @@ public interface UserRepository extends JpaRepository<User,Integer> {
 
     int countByEmail(String email);
 
-
+    @Modifying
     @Query("update User u set u.name = ?1, u.surname = ?2 , u.email = ?3, u.age = ?4, u.balance = ?5, u.password = ?6 where u.id = ?7")
     void updateUser(String name, String surname, String email,Integer age,Integer balance,String password,Integer userId);
 
